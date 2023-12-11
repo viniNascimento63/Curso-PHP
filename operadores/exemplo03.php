4faae87b6452b9e5a2e9efcb803dc499f3deff65
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-11 13:49:45 -0300

    Operadores aritméticos

---
+++ operadores/exemplo03.php
@@ -0,0 +1,9 @@
+<?php
+
+    $a = $b = 3;
+    echo $a - $b . '<br/>'; // subtração
+    echo $a * $b . '<br/>'; // multiplicação
+    echo $a / $b . '<br/>'; // divisão
+    echo $a + $b . '<br/>'; // adição
+    echo $a % $b . '<br/>'; // módulo
+    echo $a ** $b . '<br/>'; // potenciação
