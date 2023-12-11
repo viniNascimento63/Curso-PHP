e8d771600b5c9692337e4678b3c81f943b9f940d
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-11 13:39:47 -0300

    Operadores de autoatribuição

---
+++ operadores/exemplo02.php
@@ -0,0 +1,12 @@
+<?php
+
+    $valor = 0;
+    $valor += 100; // Autoatribuindo o valor da variável mais um novo valor.
+    $valor += 25;
+    echo $valor.'<br/>';
+    
+    $valor -= 10; // Autoatribuindo o valor da variável menos um novo valor.
+    echo $valor.'<br/>';
+    
+    $valor *= .9; // Autoatribuindo o valor da variável vezes um novo valor (.9 = 0,9)
+    echo $valor.'<br/>';
