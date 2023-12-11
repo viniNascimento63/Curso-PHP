1d1c9624c37fedb82fd36cb1ecc61b01a2229004
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-11 13:31:40 -0300

    Operadores de atribuição e concatenação

---
+++ operadores/exemplo01.php
@@ -0,0 +1,7 @@
+<?php
+
+    $nome = 'Lucas'; // = Atribuição
+    echo $nome.' da Silva<br/>'; // . Concatenação
+    
+    $nome .= ' Carlos'; // .= Atribuindo à $nome uma concatenação
+    echo $nome;
