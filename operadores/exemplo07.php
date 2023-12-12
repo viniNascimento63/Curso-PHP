0949826340a30738565838d94e1afee868c429c4
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-12 14:12:08 -0300

    Pré/Pós incremento/decremento

---
+++ operadores/exemplo07.php
@@ -0,0 +1,9 @@
+<?php
+
+    $a = $b = 10;
+
+    echo $a++.'<br>'; // Pós-incremento
+    echo ++$b.'<br>'; // Pré-incremento
+
+    echo $a--.'<br>'; // Pós-decremento
+    echo --$b; // Pré-decremento
