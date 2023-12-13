b5ec23dabd3deabb081e0d4a2543b7e4a23b5f41
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-13 14:50:34 -0300

    Letras maiúsculas e minúsculas

---
+++ strings/exemplo02.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nome = 'hércules da silva';
+    echo $nome.'<br>';
+
+    $nome = strtoupper($nome); // Para letras maiúsculas
+    echo $nome.'<br>';
+    
+    $nome = strtolower($nome); // Para letras minúsculas
+    echo $nome.'<br>';
+    
+    $nome = ucwords($nome); // Primeira letra cada palavra maiúscula
+    echo $nome.'<br>';
+    
+    $nome = ucfirst($nome); // Primeira letra da string maiúscula
+    echo $nome.'<br>';
