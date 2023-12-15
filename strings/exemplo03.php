63b59755cbbceada4942bcba9b6ce23b062f2d3a
Vinícius Nascimento <vini_sn@outlook.com>
2023-12-15 14:16:56 -0300

    Substituindo valores de strings com str_replace()

---
+++ strings/exemplo03.php
@@ -0,0 +1,6 @@
+<?php
+
+    $empresa = 'CursoemVideo';
+    $empresa = str_replace('o', '0', $empresa); // Subtituindo letra O por 0
+    $empresa = str_replace('e', '3', $empresa); // Substituindo letra E por 3
+    echo $empresa;
