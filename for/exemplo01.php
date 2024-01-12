c6495a68364c60534abd04c1da8f1e0cbd5b506e
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-12 11:11:22 -0300

    for

--- for/exemplo01.php
+++ for/exemplo01.php
@@ -1,4 +1,7 @@
 <?php
+    echo '<select>';
     for ($i=date('Y'); $i >= date('Y')-10 ; $i--) { 
+        echo '<option value="'.$i.'">'.$i.'</option>';
         echo $i.'<br>';
     }
+    echo '</select>';
