0871faf555969a4a22f87a214d8db53a62a9ef44
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-12 14:08:04 -0300

    array

---
+++ array/exemplo02.php
@@ -0,0 +1,17 @@
+<?php
+    $carros[0][0] = 'GM';
+    $carros[0][1] = 'Onix';
+    $carros[0][2] = 'Camaro';
+    $carros[0][3] = 'Trailblazer';
+
+    $carros[1][0] = 'Wolkswagen';
+    $carros[1][1] = 'Passat';
+    $carros[1][2] = 'Jetta';
+    $carros[1][3] = 'Tiguan';
+
+    $carros[2][0] = 'Fiat';
+    $carros[2][1] = 'Tempra';
+    $carros[2][2] = 'Bravo';
+    $carros[2][3] = 'Pálio';
+
+    echo end($carros[1]); 
