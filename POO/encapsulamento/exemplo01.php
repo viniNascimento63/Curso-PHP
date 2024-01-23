dcbdd65d7caa84bb25999057312ce5eb0a55a531
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-23 09:10:39 -0300

    Encapsulamento

---
+++ POO/encapsulamento/exemplo01.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pessoa {
+    public $nome = 'Rasmus Lerdorf';
+    protected $idade = 48;
+    private $senha = '123456';
+
+    public function verDados() {
+        echo $this->nome.'<br/>';
+        echo $this->idade.'<br/>';
+        echo $this->senha.'<br/>';
+    }
+}
+
+$pessoa = new Pessoa();
+// echo $pessoa->idade;
+$pessoa->verDados();
+?>
---
+++ POO/encapsulamento/exemplo02.php
@@ -0,0 +1,29 @@
+<?php
+
+class Pessoa {
+    public $nome = 'Rasmus Lerdorf';
+    protected $idade = 48;
+    private $senha = '123456';
+
+    public function verDados() {
+        echo $this->nome.'<br/>';
+        echo $this->idade.'<br/>';
+        echo $this->senha.'<br/>';
+    }
+}
+
+class Programador extends Pessoa {
+    public function verDados() {
+
+        echo get_class($this).'<br/>';
+
+        echo $this->nome.'<br/>';
+        echo $this->idade.'<br/>';
+        echo $this->senha.'<br/>';
+    }
+}
+
+$pessoa = new Programador();
+// echo $pessoa->idade;
+$pessoa->verDados();
+?>
