3ee0c9deac9175fa62aa862308ad240a61018fae
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-06 10:05:56 -0300

    Update config.php

--- DAO/config.php
+++ DAO/config.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function($class_name) {
-
-    $file_name = $class_name.".php";
+    $dir_class = 'class';
+    $file_name = $dir_class.DIRECTORY_SEPARATOR.$class_name.".php";
 
     if (file_exists($file_name)) {
         require_once($file_name);
