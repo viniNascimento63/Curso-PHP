1bb4183ee862e7d653f59c61f573d04f365a9317
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-06 10:06:02 -0300

    Update index.php

--- DAO/index.php
+++ DAO/index.php
@@ -1,8 +1,17 @@
 <?php
 require_once('config.php');
 
+/*
+ * Select simples da tabela
+
 $sql = new Sql();
 
 $users = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($users);
+
+*/
+
+$goku = new Usuario();
+$goku->load_by_id(3);
+echo $goku;
