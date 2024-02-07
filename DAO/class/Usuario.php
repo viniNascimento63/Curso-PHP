22e085ec12e8a7977d1105eca1230ce33b7b2a04
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-07 10:09:20 -0300

    Update Usuario.php

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -41,7 +41,7 @@ class Usuario {
     }
 
     // Carrega dados de um usuário através do id
-    public function load_by_id(int $id) {
+    public function load_by_id(int $id) : void {
         $sql = new Sql();
         $results = $sql->select('SELECT * FROM tb_usuarios WHERE id_usuario = :ID', array(
             ':ID'=>$id
@@ -59,8 +59,46 @@ class Usuario {
         }
     }
 
+    // Retorna uma lista de usuários
+    public static function get_list() : array {
+        $sql = new Sql();
+        return $sql->select('SELECT * FROM tb_usuarios ORDER BY desc_login');
+    }
+
+    // Retorna uma lista de usuários de login específico
+    public static function search(string $login) : array {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE desc_login LIKE :SEARCH ORDER BY desc_login", array(
+            ':SEARCH'=>'%'.$login.'%'
+        ));
+    }
+
+
+    public function login(string $login, string $pass) : void {
+        $sql = new Sql();
+        $results = $sql->select('SELECT * FROM tb_usuarios WHERE desc_login = :LOGIN AND desc_senha = :PASS', array(
+            ':LOGIN'=>$login,
+            ':PASS'=>$pass
+        ));
+
+        // Verifica se existe no mínimo um usuário cadastrado
+        if (isset($results[0])) {
+            $row = $results[0];
+
+            // Armazena os valores retornados nos atributos do usuário
+            $this->set_id_usuario($row['id_usuario']);
+            $this->set_desc_login($row['desc_login']);
+            $this->set_desc_senha($row['desc_senha']);
+            $this->set_data_cadastro(new DateTime($row['dt_cadastro']));            
+        } 
+        // Lança uma exceção caso não seja encontrado o login e senha
+        else {
+            throw new Exception("Login e/ou senha inválidos!");
+        }
+    }
+
     // Retorna um json quando for dado echo um objeto Usuario
-    public function __toString() {
+    public function __toString() : string {
         return json_encode(array(
             'id_usuario'=>$this->get_id_usuario(),
             'desc_login'=>$this->get_desc_login(),
