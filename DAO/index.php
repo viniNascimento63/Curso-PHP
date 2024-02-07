8f5709d54a9718e9d42fbfdd0709952e22ece3d3
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-07 10:09:22 -0300

    Update index.php

--- DAO/index.php
+++ DAO/index.php
@@ -1,17 +1,33 @@
 <?php
 require_once('config.php');
 
+/* --- Select simples da tabela --- */
 /*
- * Select simples da tabela
-
 $sql = new Sql();
-
 $users = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($users);
-
 */
 
+/* --- Selecionando um único usuário ---  */
+/*
 $goku = new Usuario();
 $goku->load_by_id(3);
 echo $goku;
+*/
+
+/* --- Carregando uma lista de usuários --- */
+/*
+$lista = Usuario::get_list();
+echo json_encode($lista);
+*/
+
+/* --- Carrega uma lista de usuários através do login --- */
+/*
+$lista = Usuario::search('freeza');
+echo json_encode($lista);
+*/
+
+/* --- Carrega um usuário se login e senha corretos --- */
+$user = new Usuario();
+$user->login('sirigueijo', '1010');
+echo $user;
