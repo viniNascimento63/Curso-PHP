475808e550dceac449b068047054e7f863e9f608
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-07 10:09:17 -0300

    Update Sql.php

--- DAO/class/Sql.php
+++ DAO/class/Sql.php
@@ -13,7 +13,7 @@ class Sql extends PDO {
         $this->conn = new PDO($dsn, $this->user, $this->pass);
     }
 
-    // EXECUTA QUERY (consulta)
+    // EXECUTA QUERY (consulta) e retorna um statement (declaração)
     public function executeQuery($rawQuery, $params = array()) {
 
         $stmt = $this->conn->prepare($rawQuery); 
@@ -25,16 +25,19 @@ class Sql extends PDO {
         return $stmt;
     }
 
-    private function setParams($statement, $parameters = array()) {
+    // Chama função que liga os parâmetros da query aos valores do array
+    private function setParams($statement, $parameters = array()) : void {
         foreach ($parameters as $key => $value) {
             $this->setParam($statement, $key, $value);
         }
     }
 
-    private function setParam($statement, $key, $value) {
+    // Liga cada parâmetro ao valor de uma chave
+    private function setParam($statement, $key, $value) : void {
         $statement->bindParam($key, $value);
     }
 
+    // Seleciona items do BD e retorna um array com os dados
     public function select($rawQuery, $params = array()) : array {
         $stmt = $this->executeQuery($rawQuery, $params);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
