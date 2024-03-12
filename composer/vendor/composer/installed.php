45e5d411edaf39f2ef2fd6b0c1084892ad995e97
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-12 09:16:04 -0300

    Composer (gerenciador de dependências)

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vinicius-nascimento63/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7128230db7a39714e7583789bbc4d5f177a5ade5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vinicius-nascimento63/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7128230db7a39714e7583789bbc4d5f177a5ade5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
