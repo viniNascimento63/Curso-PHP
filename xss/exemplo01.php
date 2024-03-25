db5825da94f8289c797ab0feda0dbbbf831625f5
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-25 09:00:45 -0300

    cross-site-scripting

---
+++ xss/exemplo01.php
@@ -0,0 +1,10 @@
+<form method="post">
+    <input type="text" name="busca">
+    <button type="submit">Enviar</button>
+</form>
+<?php
+if (isset($_POST['busca'])) {
+    echo strip_tags($_POST['busca'], '<strong>').'<br>';
+    echo htmlentities($_POST['busca']);
+}
+?>
